485146251a7b0bc9ffcd2208dbb99b25512f4aa2
Tolik <155878002+AwebdeveloperHello@users.noreply.github.com>
2025-09-23 19:28:06 +0300

    Some changes

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the element by its ID
+    const textElement = document.getElementById('MainText');
+
+    // Change the opacity to 1, triggering the CSS transition
+    textElement.style.opacity = '1';
+});
